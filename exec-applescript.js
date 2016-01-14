cd7899b269baa06e8bbc3de6121064eb7a62d14d
Jonah <jonahss@gmail.com>
2016-01-14 11:14:43 -0800

    open and close TextEdit

--- exec-applescript.js
+++ exec-applescript.js
@@ -4,7 +4,21 @@ var B = require('bluebird')
 var exec = teen_process.exec
 
 // exec('osascript', ['-e', `quit app "${appName}"`]);
-exec('osascript', ['-e', `display dialog "Hello World"`]).then(function () {
+var openTextEdit = function () {
+  return exec("open", ["/Applications/TextEdit.app"])
+}
+
+var quitTextEdit = function () {
+  return exec('osascript', ['-e', `quit app "TextEdit.app"`])
+}
+
+var delay = function () {
+  return B.delay(1000)
+}
+
+openTextEdit()
+.then(delay)
+.then(quitTextEdit).then(function () {
   console.log(`script executed`)
 }, function (e) {
   console.log(`error ${JSON.stringify(e)}`)
